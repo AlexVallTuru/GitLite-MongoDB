ba2ebf27960978869396309ac5ec6e018532a832
DESKTOP-BCBFLT7\Alex <alexvall24@gmail.com>
2023-03-28 21:03:05 +0200

    Nuevas cositas en singleton!

--- src/main/java/DAO/DocumentsDAO.java
+++ src/main/java/DAO/DocumentsDAO.java
@@ -6,29 +6,20 @@ package DAO;
 
 import Interfaces.InterfaceDAO;
 import Singleton.MongoConnection;
+import static Singleton.MongoConnection.setRepositoryName;
 import Utils.Ficheros;
 import Utils.Utils;
-
-import static Utils.Ficheros.obtenerMD5ComoString;
-import static Utils.Utils.*;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.io.File;
-<<<<<<< HEAD
 import java.io.IOException;
-import java.sql.Timestamp;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-=======
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
->>>>>>> develop
 import java.util.ArrayList;
-import java.util.Date;
 
 import org.bson.Document;
 
@@ -46,6 +37,7 @@ public class DocumentsDAO implements InterfaceDAO {
     MongoClient connection = MongoConnection.getInstance();
     MongoDatabase repository = null;
     String idRemot = null;
+    MongoDatabase db = MongoConnection.getDataBase();
 
     /**
      * Crea un repositori a la BBDD remota amb una ruta indicada per l'usuari.
@@ -76,7 +68,7 @@ public class DocumentsDAO implements InterfaceDAO {
 
             //Canvia els separadors de ruta per _
             ruta = ruta.replace(fileSeparator, "_");
-
+            setRepositoryName(ruta);
             //Comprobem si la col·lecció ja existeix
             boolean collectionExists = repository.listCollectionNames()
                     .into(new ArrayList<>()).contains(ruta);
@@ -162,47 +154,31 @@ public class DocumentsDAO implements InterfaceDAO {
     }
 
     /**
-        System.out.print("\t\t\t\t C O M P A R A N D O\n");
-        //Procés que compara els continguts dels fitxers locals amb els seus equivalents remots i informa a l’usuari
-        // + FITXER de un fitxer en concret
-        String carpetaConArchivos = aniadirBarraFinal(ruta.get(0));
-        File carpeta = new File(aniadirBarraFinal(ruta.get(0)));
-        File[] archivos = carpeta.listFiles();
-        File file = new File(ruta.get(1));
-        String archivoAComparar = ruta.get(1);
-        for (File archivo : archivos) {
-            if (archivo.isFile() && archivo.getName().endsWith(".txt")) {
-                if (file.lastModified() == archivo.lastModified()) {
-                    System.out.print(file.getName() + " & " + archivo.getName() +
-                            "\t\t\t\t FECHA IGUAL");
-
-                    String checksum = obtenerMD5ComoString(archivoAComparar);
-                    String checksumAComparar = obtenerMD5ComoString(carpetaConArchivos + archivo.getName());
-                    if (checksum.equals(checksumAComparar)) {
-                        System.out.print(" CONTENIDO IGUAL\n");
-                    } else {
-                        System.out.print(" CONTENIDO DIFERENTE\n");
-                    }
-                }
-                else{
-                    System.out.print(file.getName() + " & " + archivo.getName() +
-                            "\t\t\t\t FECHA DIFERENTE");
-                    String checksum = obtenerMD5ComoString(archivoAComparar);
-                    String checksumAComparar = obtenerMD5ComoString(carpetaConArchivos + archivo.getName());
-                    if (checksum.equals(checksumAComparar)) {
-                        System.out.print(" CONTENIDO IGUAL\n");
-                    } else {
-                        System.out.print(" CONTENIDO DIFERENTE\n");
-                    }
-                }
-            }
-        }
-        System.out.print("\t\t\t\t - - - - - - - - - -\n");
-    }
-**/
-
-
-
+     * System.out.print("\t\t\t\t C O M P A R A N D O\n"); //Procés que compara
+     * els continguts dels fitxers locals amb els seus equivalents remots i
+     * informa a l’usuari // + FITXER de un fitxer en concret String
+     * carpetaConArchivos = aniadirBarraFinal(ruta.get(0)); File carpeta = new
+     * File(aniadirBarraFinal(ruta.get(0))); File[] archivos =
+     * carpeta.listFiles(); File file = new File(ruta.get(1)); String
+     * archivoAComparar = ruta.get(1); for (File archivo : archivos) { if
+     * (archivo.isFile() && archivo.getName().endsWith(".txt")) { if
+     * (file.lastModified() == archivo.lastModified()) {
+     * System.out.print(file.getName() + " & " + archivo.getName() + "\t\t\t\t
+     * FECHA IGUAL");
+     *
+     * String checksum = obtenerMD5ComoString(archivoAComparar); String
+     * checksumAComparar = obtenerMD5ComoString(carpetaConArchivos +
+     * archivo.getName()); if (checksum.equals(checksumAComparar)) {
+     * System.out.print(" CONTENIDO IGUAL\n"); } else { System.out.print("
+     * CONTENIDO DIFERENTE\n"); } } else{ System.out.print(file.getName() + " &
+     * " + archivo.getName() + "\t\t\t\t FECHA DIFERENTE"); String checksum =
+     * obtenerMD5ComoString(archivoAComparar); String checksumAComparar =
+     * obtenerMD5ComoString(carpetaConArchivos + archivo.getName()); if
+     * (checksum.equals(checksumAComparar)) { System.out.print(" CONTENIDO
+     * IGUAL\n"); } else { System.out.print(" CONTENIDO DIFERENTE\n"); } } } }
+     * System.out.print("\t\t\t\t - - - - - - - - - -\n"); }
+*
+     */
     @Override
     public void cloneRepository(String file, String date) {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -210,31 +186,22 @@ public class DocumentsDAO implements InterfaceDAO {
 
     @Override
     public void compareFiles(String file, boolean containsDetails) throws ParseException {
-        // Configurar la conexión a la base de datos
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
 
-        // Obtener la base de datos
-        MongoDatabase database = mongoClient.getDatabase("GETBD");
-
-        // Obtener una referencia a la colección
-        MongoCollection<Document> collection = database.getCollection("Users");
+        
+       repositoryPath --> Se tiene que concatenar con file !!!!
 
         // Construir la consulta
         Document query = new Document("path", "\\Users\\avall\\Desktop\\primero.txt");
 
         // Obtener el primer documento que cumple con la consulta
-        Document documento = collection.find(query).first();
-
+        //Document documento = colection.find(query).first();
+        
+        System.out.println("El contenido del documento es: " + documento);
 
         // Imprimir el documento obtenido
         System.out.println(documento);
 
-        // Cerrar la conexión a la base de datos
-        mongoClient.close();
-
-
         // TODO LO ANTERIOR ES PAJA
-
         File bddFile;
         try {
             bddFile = Utils.documentToFile(documento);
@@ -243,13 +210,18 @@ public class DocumentsDAO implements InterfaceDAO {
             throw new RuntimeException(e);
         }
         File localFile = new File("\\Users\\avall\\Desktop\\primero.txt");
-        System.out.print("El contenido del timestamps de la baseDeDatos es de " +  bddFile.lastModified() + "\n"
+        System.out.print("El contenido del timestamps de la baseDeDatos es de " + bddFile.lastModified() + "\n"
                 + "El contenido del timestamps local es de " + localFile.lastModified());
-        if (bddFile.lastModified() == localFile.lastModified()){
+        if (bddFile.lastModified() == localFile.lastModified()) {
             System.out.print("\nSON IGUALES");
         } else {
             System.out.print("\nSon diferentes");
         }
+    }
+
+    @Override
+    public void cloneRepository(String file) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
 }
--- src/main/java/Singleton/MongoConnection.java
+++ src/main/java/Singleton/MongoConnection.java
@@ -5,7 +5,11 @@
 package Singleton;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.bson.Document;
 
 public class MongoConnection {
 
@@ -14,11 +18,14 @@ public class MongoConnection {
     private static MongoConnection mongoClient;
     private static String repositoryName;
     private static Path repositoryPath;
+    private static MongoDatabase dataBase;
 
     private MongoConnection() {
 
         this.client = new MongoClient("localhost", 27017);
-
+        this.dataBase = client.getDatabase("GETBD");
+        this.repositoryName = "Users_Alex_Desktop";
+        this.repositoryPath = Paths.get("\\Users\\Alex\\Desktop");
     }
 
     public static MongoClient getInstance() {
@@ -32,12 +39,21 @@ public class MongoConnection {
         repositoryFolder = repository;
     }
 
-    public static String setRepositoryName(Path repositoryPath) {
-       String ret = null;
-       return ret;
+    public static void setRepositoryName(String repositoryName) {
+        repositoryName = repositoryName;
+    }
+
+    public static void setRepositoryPath(Path repositoryPath) {
+        //Identificar el sistema operativo y en base a esto introducir
+        //una barra inicial acompañada de otras que substituyan la barrabaja
+        repositoryPath = repositoryPath;
     }
 
     public static String getRepository() {
         return repositoryFolder;
+    }
+
+    public static MongoDatabase getDataBase() {
+        return dataBase;
     }
 }
